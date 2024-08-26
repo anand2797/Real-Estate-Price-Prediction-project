d45e8bc6c907c441bb49527f52fa2e92aa1c7e6d
Anand Talware <anandtalware27@gmail.com>
2024-08-26 19:23:44 +0530

    Correction in logger.py

--- src/real_estate_price/logger.py
+++ src/real_estate_price/logger.py
@@ -2,12 +2,12 @@ import os
 import logging
 from datetime import datetime
 
-log_file = f"{datetime.now().strftime(%d_%m_%Y_%H_%M_%S)}.log"
+log_file = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 
 log_path = os.path.join(os.getcwd(), "logs")
 
 # create directory for logs for store log file
-os.makedirs(lo_gpath, exist_ok=True)
+os.makedirs(log_path, exist_ok=True)
 
 #join directory and file to create full path
 log_file_path = os.path.join(log_path, log_file)
